fbaaaee16576bb428960b7e1906103f51bab4040
gregoryvanko <gregory.vankoninckxloo@gmail.com>
2023-04-22 21:23:35 +0200

    Update exemple.js

--- exemple.js
+++ exemple.js
@@ -1,6 +1,6 @@
 const Option = {
     Port:5000,
-    Name:"AquagreenDev",
+    Name:"NanoxDev",
     Debug: true,
     SplashScreenFilePath: __dirname + "/Frontend/SplashScreen/SplashScreen.html",
     MongoDbUrl: "mongodb://mongodev:27017"
